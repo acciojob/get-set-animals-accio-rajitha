25d65cc73a2f6f67e3a6aad7ad325808e20150bc
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-28 16:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this.species = species;
+		this._species = species;
 	}
 	get species(){
-		return this.species;
+		return this._species;
 	}
 	makeSound() {
-        console.log(`The ${this.species} makes a sound`);
+        console.log(`The ${this._species} makes a sound`);
     }
 }
 
@@ -23,15 +23,9 @@ class Cat extends Animal {
 		console.log("purr");
 	}
 }
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
 
-// Do not change the code below this line
+
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
